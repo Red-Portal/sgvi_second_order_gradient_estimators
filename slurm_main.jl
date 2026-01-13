c55636e757d2be16d5c92239e47502dd4ac8a9cb
Kyurae Kim <kyrkim@seas.upenn.edu>
2026-01-13 17:10:28 -0500

    add no gpu option to slurm

--- slurm_main.jl
+++ slurm_main.jl
@@ -1,12 +1,13 @@
 #!/usr/bin/env julia
-#SBATCH --output=logs/%x_%j.out     # Stdout goes to logs/jobname_jobid.out
-#SBATCH --error=logs/%x_%j.err      # Stderr goes to logs/jobname_jobid.err
-#SBATCH --partition=genoa-std-mem   # Queue to submit to
-#SBATCH --ntasks=4                  # Number of tasks (usually one per process)
-#SBATCH --cpus-per-task=1           # Number of CPU cores per task
-#SBATCH --mem=256G                  # Memory allocation
-#SBATCH --time=00:05:00             # Maximum runtime (hh:mm:ss)
-#SBATCH --qos=normal                 
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+#SBATCH --partition=genoa-std-mem
+#SBATCH --ntasks=4
+#SBATCH --gpus=0
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=256G
+#SBATCH --time=00:05:00
+#SBATCH --qos=normal
 
 using Distributed, SlurmClusterManager
 

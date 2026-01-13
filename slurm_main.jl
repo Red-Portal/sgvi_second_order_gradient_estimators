428381b37cdbe21969275d19cce0fc3100451932
Kyurae Kim <kyrkim@seas.upenn.edu>
2026-01-13 17:09:22 -0500

    add default slurm config in slurm_main

--- slurm_main.jl
+++ slurm_main.jl
@@ -1,4 +1,12 @@
 #!/usr/bin/env julia
+#SBATCH --output=logs/%x_%j.out     # Stdout goes to logs/jobname_jobid.out
+#SBATCH --error=logs/%x_%j.err      # Stderr goes to logs/jobname_jobid.err
+#SBATCH --partition=genoa-std-mem   # Queue to submit to
+#SBATCH --ntasks=4                  # Number of tasks (usually one per process)
+#SBATCH --cpus-per-task=1           # Number of CPU cores per task
+#SBATCH --mem=256G                  # Memory allocation
+#SBATCH --time=00:05:00             # Maximum runtime (hh:mm:ss)
+#SBATCH --qos=normal                 
 
 using Distributed, SlurmClusterManager
 
